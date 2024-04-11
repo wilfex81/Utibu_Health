c8f68debcb3524db747eeb113659c330e7884e17
wilfex <wilfex81@gmail.com>
2024-04-11 19:59:03 +0300

    update docs for stkpush

--- orders/urls.py
+++ orders/urls.py
@@ -14,6 +14,5 @@ urlpatterns = [
     path('medications/<int:pk>/', MedicationDetailAPIView.as_view(), name='medication-detail'),
     path('statement/<int:pk>/', StatementDetailAPIView.as_view(), name='statement-detail'),
     path('accesstoken/', views.get_access_token, name='get_access_token'),
-    path('stkpush/', views.initiate_stk_push, name='initiate_stk_push'),
     path('stkpush/', STKPushAPiView.as_view(),name = 'stk_push'),
 ]
